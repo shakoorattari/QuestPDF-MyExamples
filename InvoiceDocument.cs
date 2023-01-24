faaf28ace02d5681276c83621e0294f99f87dd40
Shakoor Hussain Attari <binmushtaq@gmail.com>
2023-01-24 23:22:30 +0400

    Arabic Text mixed with english

--- InvoiceDocument.cs
+++ InvoiceDocument.cs
@@ -1,4 +1,5 @@
-using System.Linq;
+﻿using System.Linq;
+using System.Reflection.Metadata;
 using QuestPDF.Drawing;
 using QuestPDF.Fluent;
 using QuestPDF.Helpers;
@@ -19,21 +20,124 @@ namespace QuestPDF.ExampleInvoice
 
         public void Compose(IDocumentContainer container)
         {
-            container
-                .Page(page =>
+            container.Page(page =>
+            {
+                page.Size(PageSizes.A4);
+                page.Margin(20);
+                page.PageColor(Colors.Grey.Lighten1);
+
+                page.DefaultTextStyle(x => x.FontFamily("Calibri").FontSize(20));
+                page.ContentFromRightToLeft();
+
+                page.Content().Column(column =>
                 {
-                    page.Margin(50);
-                    
-                    page.Header().Element(ComposeHeader);
-                    page.Content().Element(ComposeContent);
-                    
-                    page.Footer().AlignCenter().Text(text =>
+                    column.Spacing(20);
+
+                    column.Item()
+                        .Text("مثال على الفاتورة") // example invoice
+                        .FontSize(32).FontColor(Colors.Blue.Darken2).SemiBold();
+
+                    column.Item().Table(table =>
                     {
-                        text.CurrentPageNumber();
-                        text.Span(" / ");
-                        text.TotalPages();
+                        table.ColumnsDefinition(columns =>
+                        {
+                            columns.RelativeColumn();
+                            columns.ConstantColumn(75);
+                            columns.ConstantColumn(100);
+                        });
+
+                        table.Cell().Element(HeaderStyle).Text("وصف السلعة"); // item description
+                        table.Cell().Element(HeaderStyle).Text("كمية"); // quantity
+                        table.Cell().Element(HeaderStyle).Text("سعر"); // price
+
+                        var items = new[]
+                        {
+                "دورة البرمجة", // programming course
+                "دورة تصميم الرسومات", // graphics design course
+                "تحليل وتصميم الخوارزميات", // analysis and design of algorithms
+                "دورة البرمجة", // programming course
+                    "دورة تصميم الرسومات", // graphics design course
+                    "تحليل وتصميم الخوارزميات", // analysis and design of algorithms
+                "دورة البرمجة", // programming course
+                    "دورة تصميم الرسومات", // graphics design course
+                    "تحليل وتصميم الخوارزميات", // analysis and design of algorithms
+                "دورة البرمجة", // programming course
+                    "دورة تصميم الرسومات", // graphics design course
+                    "تحليل وتصميم الخوارزميات", // analysis and design of algorithms
+                "دورة البرمجة", // programming course
+                    "دورة تصميم الرسومات", // graphics design course
+                    "تحليل وتصميم الخوارزميات", // analysis and design of algorithms
+                "دورة البرمجة", // programming course
+                    "دورة تصميم الرسومات", // graphics design course
+                    "تحليل وتصميم الخوارزميات", // analysis and design of algorithms
+                "دورة البرمجة", // programming course
+                    "دورة تصميم الرسومات", // graphics design course
+                    "تحليل وتصميم الخوارزميات", // analysis and design of algorithms
+                "دورة البرمجة", // programming course
+                    "دورة تصميم الرسومات", // graphics design course
+                    "تحليل وتصميم الخوارزميات", // analysis and design of algorithms
+                "دورة البرمجة", // programming course
+                    "دورة تصميم الرسومات", // graphics design course
+                    "تحليل وتصميم الخوارزميات", // analysis and design of algorithms
+                "دورة البرمجة", // programming course
+                    "دورة تصميم الرسومات", // graphics design course
+                    "تحليل وتصميم الخوارزميات", // analysis and design of algorithms
+                "دورة البرمجة", // programming course
+                    "دورة تصميم الرسومات", // graphics design course
+                    "تحليل وتصميم الخوارزميات", // analysis and design of algorithms
+                "دورة البرمجة", // programming course
+                    "دورة تصميم الرسومات", // graphics design course
+                    "تحليل وتصميم الخوارزميات", // analysis and design of algorithms
+                "دورة البرمجة", // programming course
+                    "دورة تصميم الرسومات", // graphics design course
+                    "تحليل وتصميم الخوارزميات", // analysis and design of algorithms
+                "دورة البرمجة", // programming course
+                    "دورة تصميم الرسومات", // graphics design course
+                    "تحليل وتصميم الخوارزميات", // analysis and design of algorithms
+                "دورة البرمجة", // programming course
+                    "دورة تصميم الرسومات", // graphics design course
+                    "تحليل وتصميم الخوارزميات", // analysis and design of algorithms
+                "دورة البرمجة", // programming course
+                    "دورة تصميم الرسومات", // graphics design course
+                    "تحليل وتصميم الخوارزميات", // analysis and design of algorithms
+                "دورة البرمجة", // programming course
+                    "دورة تصميم الرسومات", // graphics design course
+                    "تحليل وتصميم الخوارزميات", // analysis and design of algorithms
+                "دورة البرمجة", // programming course
+                    "دورة تصميم الرسومات", // graphics design course
+                    "تحليل وتصميم الخوارزميات", // analysis and design of algorithms
+            };
+
+                        foreach (var item in items)
+                        {
+                            var price = Placeholders.Random.NextDouble() * 100;
+
+                            table.Cell().Text(item);
+                            table.Cell().Text(Placeholders.Random.Next(1, 10));
+                            table.Cell().Text($"USD${price:F2}");
+                        }
+
+                        static IContainer HeaderStyle(IContainer x) => x.BorderBottom(1).PaddingVertical(5);
                     });
                 });
+            });
+
+
+            //container
+            //    .Page(page =>
+            //    {
+            //        page.Margin(50);
+
+            //        page.Header().Element(ComposeHeader);
+            //        page.Content().Element(ComposeContent);
+
+            //        page.Footer().AlignCenter().Text(text =>
+            //        {
+            //            text.CurrentPageNumber();
+            //            text.Span(" / ");
+            //            text.TotalPages();
+            //        });
+            //    });
         }
 
         void ComposeHeader(IContainer container)
@@ -45,30 +149,30 @@ namespace QuestPDF.ExampleInvoice
                     Column
                         .Item().Text($"Invoice #{Model.InvoiceNumber}")
                         .FontSize(20).SemiBold().FontColor(Colors.Blue.Medium);
-                    
+
                     Column.Item().Text(text =>
                     {
                         text.Span("Issue date: ").SemiBold();
                         text.Span($"{Model.IssueDate:d}");
                     });
-                    
+
                     Column.Item().Text(text =>
                     {
                         text.Span("Due date: ").SemiBold();
                         text.Span($"{Model.DueDate:d}");
                     });
                 });
-                
+
                 row.ConstantItem(100).Height(50).Placeholder();
             });
         }
 
         void ComposeContent(IContainer container)
         {
-            container.PaddingVertical(40).Column(column => 
+            container.PaddingVertical(40).Column(column =>
             {
                 column.Spacing(20);
-                
+
                 column.Item().Row(row =>
                 {
                     row.RelativeItem().Component(new AddressComponent("From", Model.SellerAddress));
@@ -89,7 +193,7 @@ namespace QuestPDF.ExampleInvoice
         void ComposeTable(IContainer container)
         {
             var headerStyle = TextStyle.Default.SemiBold();
-            
+
             container.Table(table =>
             {
                 table.ColumnsDefinition(columns =>
@@ -100,7 +204,7 @@ namespace QuestPDF.ExampleInvoice
                     columns.RelativeColumn();
                     columns.RelativeColumn();
                 });
-                
+
                 table.Header(header =>
                 {
                     header.Cell().Text("#");
@@ -108,20 +212,18 @@ namespace QuestPDF.ExampleInvoice
                     header.Cell().AlignRight().Text("Unit price").Style(headerStyle);
                     header.Cell().AlignRight().Text("Quantity").Style(headerStyle);
                     header.Cell().AlignRight().Text("Total").Style(headerStyle);
-                    
+
                     header.Cell().ColumnSpan(5).PaddingTop(5).BorderBottom(1).BorderColor(Colors.Black);
                 });
-                
+
                 foreach (var item in Model.Items)
                 {
-                    var index = Model.Items.IndexOf(item) + 1;
-                    
-                    table.Cell().Element(CellStyle).Text($"{index}");
+                    table.Cell().Element(CellStyle).Text(Model.Items.IndexOf(item) + 1);
                     table.Cell().Element(CellStyle).Text(item.Name);
                     table.Cell().Element(CellStyle).AlignRight().Text($"{item.Price}$");
-                    table.Cell().Element(CellStyle).AlignRight().Text($"{item.Quantity}");
+                    table.Cell().Element(CellStyle).AlignRight().Text(item.Quantity);
                     table.Cell().Element(CellStyle).AlignRight().Text($"{item.Price * item.Quantity}$");
-                    
+
                     static IContainer CellStyle(IContainer container) => container.BorderBottom(1).BorderColor(Colors.Grey.Lighten2).PaddingVertical(5);
                 }
             });
@@ -129,7 +231,7 @@ namespace QuestPDF.ExampleInvoice
 
         void ComposeComments(IContainer container)
         {
-            container.ShowEntire().Background(Colors.Grey.Lighten3).Padding(10).Column(column => 
+            container.ShowEntire().Background(Colors.Grey.Lighten3).Padding(10).Column(column =>
             {
                 column.Spacing(5);
                 column.Item().Text("Comments").FontSize(14).SemiBold();
@@ -137,7 +239,7 @@ namespace QuestPDF.ExampleInvoice
             });
         }
     }
-    
+
     public class AddressComponent : IComponent
     {
         private string Title { get; }
@@ -148,7 +250,7 @@ namespace QuestPDF.ExampleInvoice
             Title = title;
             Address = address;
         }
-        
+
         public void Compose(IContainer container)
         {
             container.ShowEntire().Column(column =>
@@ -156,8 +258,8 @@ namespace QuestPDF.ExampleInvoice
                 column.Spacing(2);
 
                 column.Item().Text(Title).SemiBold();
-                column.Item().PaddingBottom(5).LineHorizontal(1); 
-                
+                column.Item().PaddingBottom(5).LineHorizontal(1);
+
                 column.Item().Text(Address.CompanyName);
                 column.Item().Text(Address.Street);
                 column.Item().Text($"{Address.City}, {Address.State}");
--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
 using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
 using QuestPDF.Previewer;
 
 namespace QuestPDF.ExampleInvoice
@@ -15,21 +17,72 @@ namespace QuestPDF.ExampleInvoice
         /// </summary>
         static void Main(string[] args)
         {
-            var model = InvoiceDocumentDataSource.GetInvoiceDetails();
-            var document = new InvoiceDocument(model);
+            var document = Document.Create(container =>
+            {
+                container.Page(page =>
+                {
+                    page.Size(PageSizes.A4);
+                    page.Margin(2, Unit.Centimetre);
+                    page.PageColor(Colors.White);
+                    page.DefaultTextStyle(x => x.FontSize(20).FontFamily("Arial"));
+
+                    page.Header()
+                        .AlignRight()
+                        .Text("بسم الله الرحمن الرحيم")
+                        .DirectionFromRightToLeft()
+                        .SemiBold().FontSize(36).FontColor(Colors.Blue.Medium);
+
+                    page.Content()
+                        .PaddingVertical(1, Unit.Centimetre)
+                        .ContentFromRightToLeft()
+                        .Column(x =>
+                        {
+                            x.Spacing(20);
+
+                            x.Item().AlignRight().Text(t =>
+                            {
+                                t.Span("البرمجة بلغة").DirectionFromRightToLeft();
+                                t.Span(" C# ");
+                                t.Span("ضمن اطار عمل").DirectionFromRightToLeft();
+                                t.Span(" .Net Core ");
+                                t.Span("ممتع").DirectionFromRightToLeft();
+                            });
+                            x.Item().Image(Placeholders.Image(200, 100));
+                        });
+
+                    page.Footer()
+                        .AlignCenter()
+                        .ContentFromRightToLeft()
+                        .Text(x =>
+                        {
+                            x.Span("صفحة ");
+                            x.CurrentPageNumber();
+                        });
+                });
+            });
+
+            var pdfFile = document.GeneratePdf();
+            File.WriteAllBytes("temp.pdf", pdfFile);
+
+            //var model = InvoiceDocumentDataSource.GetInvoiceDetails();
+            //var document = new InvoiceDocument(model);
+
+
+
+
 
             // Generate PDF file and show it in the default viewer
             //GenerateDocumentAndShow(document);
-            
+
             // Or open the QuestPDF Previewer and experiment with the document's design
             // in real-time without recompilation after each code change
-            document.ShowInPreviewer();
+            //document.ShowInPreviewer();
         }
 
         static void GenerateDocumentAndShow(InvoiceDocument document)
         {
             const string filePath = "invoice.pdf";
-            
+
             document.GeneratePdf(filePath);
 
             var process = new Process
